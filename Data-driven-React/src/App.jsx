1442b3aedb32ab75e6336efbbbc76cde003c6c80
Simone Ghosh <simone.ghoshbd@gmail.com>
2026-01-27 10:39:11 -0500

    Add files via upload

---
+++ Data-driven-React/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import Header from "./components/Header";
+import Entry from "./components/Entry";
+import data from "./data";
+
+export default function App() {
+
+  const entries = data.map(entry => {
+    return (
+      <Entry 
+             key={entry.id}
+             {...entry}
+      />
+    )
+  })
+  
+  return (
+    <>
+      <Header />
+      <main className="container">
+        {entries}
+      </main>
+    </>
+  );
+}
