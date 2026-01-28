42bad475cc3a7e30ac032c8194168df904f223e9
Simone Ghosh <simone.ghoshbd@gmail.com>
2026-01-28 10:16:54 -0500

    Add files via upload

---
+++ Chef-Claude/src/components/header.jsx
@@ -0,0 +1,10 @@
+import chefClaudeIcon from "../images/Chef Claude Icon.png";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={chefClaudeIcon} alt="logo" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+}
